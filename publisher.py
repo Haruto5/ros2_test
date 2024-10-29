2d6e9eb94fcd656110c83eee08fe8d98daab2c1e
hakawazu <96673460+Haruto5@users.noreply.github.com>
2024-10-29 11:07:30 +0900

    Create publisher.py

---
+++ publisher.py
@@ -0,0 +1,25 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class HelloWorldPublisher(Node):
+    def __init__(self):
+        super().__init__('hello_world_publisher')
+        self.publisher_ = self.create_publisher(String, 'hello_topic', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World'
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = HelloWorldPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
