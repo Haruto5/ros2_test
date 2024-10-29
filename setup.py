3a0ff95089272358ee96a9c0c766ab59843a9728
hakawazu <96673460+Haruto5@users.noreply.github.com>
2024-10-29 15:23:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 from setuptools import setup
+import os
+from glob import glob
 
-package_name = 'hello_world'
+package_name = 'camera'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    data_files=[
+        # パッケージに必要なファイルをインストールする
+        (os.path.join('share', package_name), ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A simple Hello World publisher and subscriber',
+    maintainer='Your Name',
+    maintainer_email='your_email@example.com',
+    description='ROS2 package for publishing and subscribing float arrays',
     license='Apache License 2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'publisher = hello_world.publisher:main',
-            'subscriber = hello_world.subscriber:main',
+            # 実行ファイルの登録
+            'publisher = camera.publisher:main',
+            'subscriber = camera.subscriber:main',
         ],
     },
 )
